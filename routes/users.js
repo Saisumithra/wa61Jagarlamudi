f2a097319035953ffdcc0f7fce3a6b898def661b
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2021-03-24 19:14:49 -0500

    updated user file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var hs = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  hs= hs+1;
+  res.send('User accesses are:' +hs);
 });
 
 module.exports = router;
