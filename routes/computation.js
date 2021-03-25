28da9da2d41e8a2715dd5c5caa3c3445c5da3ac4
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2021-03-24 20:08:01 -0500

    changes

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-let someNumber=Math.floor(Math.random()*100)
-res.render('sh', { result:'log applied to '+someNumber+' is '+Math.ceil(someNumber)});
+let someNumber=(Math.random()*100)
+res.render('sh', { result:'ceil applied to '+someNumber+' is '+Math.ceil(someNumber)});
 });
 module.exports = router;
